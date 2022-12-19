96933b2fe1562f71d574f6bbb9ce25b340320f80
vitakikshvets232003@gmail.com <vitakikshvets232003@gmail.com>
2022-12-19 18:24:20 +0200

    lab1

---
+++ laborka1/laborka1/laborka1.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "Ukr");
+	int Var, Var2;
+	cout << "Введите первое чесло :";
+	cin >> Var;
+
+	cout << "Введите второе число :";
+	cin >> Var2;
+	cout << "Число 1 = " << Var << "Число 2= " << Var2 << endl;
+}
