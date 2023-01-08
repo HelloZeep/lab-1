4bddb674994ceee4adcd013788eab95bbbdf3096
HelloZeep <vitakikshvets232003@gmail.com>
2023-01-09 00:21:00 +0200

    laborka 2

---
+++ Laba2/Laba2/Laba2.cpp
@@ -0,0 +1,21 @@
+﻿#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 4, c, d = 3, f;
+
+
+    c = a + b;
+    cout << "10 + 4 = " << c << "\n";
+    f = d + c;
+
+    cout << c << " + " << d << " = " << f;
+
+
+
+
+
+}
+
