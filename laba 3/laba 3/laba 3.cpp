ea23700ca1843e57cebf6186e983bd3d4906a951
HelloZeep <vitakikshvets232003@gmail.com>
2023-01-09 00:44:27 +0200

    laba3

---
+++ laba 3/laba 3/laba 3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	cout << (10 > 20) << endl;
+	cout << (10 < 20) << endl;
+	cout << (10 == 20) << endl;
+	cout << (10 != 20) << endl;
+
+}
+
+// > число більши число
+// < менше число 
+// == дорівнює
+// != не дорівнює
+// 0 не правда; 1 правда 
