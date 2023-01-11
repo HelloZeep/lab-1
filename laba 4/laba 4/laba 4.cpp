4afdf2c60209c85d5ce64fc5f63833deca899124
HelloZeep <vitakikshvets232003@gmail.com>
2023-01-12 01:56:57 +0200

    laba4

---
+++ laba 4/laba 4/laba 4.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+
+void main()
+{
+	setlocale(LC_ALL, "Ukr");
+	int arr[4];
+
+
+	arr[0] = 10;
+	arr[1] = 53242;
+	arr[2] = 976;
+	arr[3] = 4567876;
+
+	cout << arr[0] << endl;
+	cout << arr[1] << endl;
+	cout << arr[2] << endl;
+	cout << arr[3] << endl;
+
+
+
+
+}
