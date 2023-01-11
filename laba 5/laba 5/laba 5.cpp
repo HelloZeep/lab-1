2232b6a96df86dc50c35f179fb74aedff8ee7cd4
HelloZeep <vitakikshvets232003@gmail.com>
2023-01-12 01:58:22 +0200

    laba5

---
+++ laba 5/laba 5/laba 5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+    int a = 10;
+    int* pa = &a;
+
+    cout << "a = " << a << endl;
+    cout << "*pa = " << *pa << endl;
+
+    *pa = 5;
+
+
+
+
+    return 0;
+}
